a95add60333b49ef45e3237e2d8261c6361d6ff6
vinaykumarp16 <vinaykumar.p1995v@gmail.com>
2023-08-23 13:24:56 +0530

    fix(build_fail): eliminating build fail

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
